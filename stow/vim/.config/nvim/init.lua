f8a0d0949a520b0d13bfc1110f2b0a09ff16f44c
DmitriySud <dyusudakov@edu.hse.ru>
2025-03-13 01:43:39 +0300

    block selection via treesitter and search hotkey

--- stow/vim/.config/nvim/init.lua
+++ stow/vim/.config/nvim/init.lua
@@ -40,3 +40,12 @@ vim.cmd('let &packpath = &runtimepath')
 vim.cmd('source ~/.vimrc')
 
 require('settings.plugins')
+
+local ts_select = require("nvim-treesitter.incremental_selection")
+
+vim.keymap.set("v", "<M-k>", ts_select.node_incremental, { silent = true })
+vim.keymap.set("v", "<M-j>", ts_select.node_decremental, { silent = true })
+
+
+vim.keymap.set("v", "<leader>s", "<Esc>/\\%V", { silent = false })
+
