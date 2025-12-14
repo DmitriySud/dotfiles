1c2c46fbf90362a0e124b401e68a6431bfc8d570
DmitriySud <dmitriy.sudakov2001@gmail.com>
2025-12-15 00:27:50 +0300

    remove old config files; move nixosdump/nixos -> nixos

--- stow/vim/.config/nvim/lua/settings/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-    text_transformt = require('settings.plugins.text-transform'),
-    lualine = require('settings.plugins.lualine'),
-    treesitter = require('settings.plugins.treesitter'),
-    catppuccin = require('settings.plugins.catppuccin'),
-    coc = require('settings.plugins.coc-nvim'),
-    autopairs = require('settings.plugins.autopairs'),
-    ibl = require('settings.plugins.ibl'),
-}
