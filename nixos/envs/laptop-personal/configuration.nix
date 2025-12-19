2e2c05a4c1de3a83eb948e2761869669bc00f616
DmitriySud <dmitriy.sudakov2001@gmail.com>
2025-12-19 16:17:42 +0300

    add laptop-personal configuration

---
+++ nixos/envs/laptop-personal/configuration.nix
@@ -0,0 +1,10 @@
+{ config, lib,  pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+      ../configuration-base.nix
+    ];
+}
+
---
+++ nixos/envs/laptop-personal/home.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+{
+    imports = [
+        ../home-base.nix
+    ];
+
+    my.hyprland = {
+        enable = true;
+        monitors = [
+          "eDP-1, preferred, 0x0, 1"
+          "DP-1, preffered, -2560x0, 1"
+        ];
+
+        workspaces = [
+            "1, DP-1"
+            "2, DP-1"
+            "3, DP-1"
+            "4, DP-1"
+            "5, DP-1"
+            "6, eDP-1"
+            "7, eDP-1"
+            "8, eDP-1"
+            "9, eDP-1"
+            "10, eDP-1"
+        ];
+    };
+}
