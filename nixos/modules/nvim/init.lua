9b1b51bb28deb0a68f115e3d69f1e37902b320b0
Dmitriy Sudakov <dmitriy.sudakov2001@gmail.com>
2025-12-28 16:44:54 +0300

    port neovim settings from dev

--- nixos/modules/nvim/init.lua
+++ nixos/modules/nvim/init.lua
@@ -1,39 +1,4 @@
-
 local vim = vim
---- local Plug = vim.fn['plug#']
---- 
---- vim.call('plug#begin')
---- 
---- --- Files navigation
---- Plug('mhinz/vim-startify')
---- Plug('nvim-lua/plenary.nvim')
---- Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.8' })
---- Plug('scrooloose/nerdtree', { ['on'] =  'NERDTreeToggle' })
---- 
---- --- Code navigation
---- Plug('easymotion/vim-easymotion')
---- 
---- -- Code manipulation
---- Plug('preservim/nerdcommenter')
---- Plug 'windwp/nvim-autopairs'
---- 
---- -- Code complete and explore
---- Plug('neoclide/coc.nvim', {['branch'] = 'release'})
---- Plug('liuchengxu/vista.vim')
---- 
---- -- Code appearance
---- Plug 'nvim-tree/nvim-web-devicons'
---- 
---- Plug('catppuccin/nvim', { ['as'] = 'catppuccin'  })
---- 
---- Plug 'nvim-lualine/lualine.nvim'
---- Plug "lukas-reineke/indent-blankline.nvim"
---- 
---- Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate'})
---- 
---- Plug('chenasraf/text-transform.nvim', { ['tag'] = 'stable' })
---- 
---- vim.call('plug#end')
 
 vim.cmd('set runtimepath^=~/.vim runtimepath+=~/.vim/after')
 vim.cmd('let &packpath = &runtimepath')
@@ -45,7 +10,15 @@ local ts_select = require("nvim-treesitter.incremental_selection")
 
 vim.keymap.set("v", "<M-k>", ts_select.node_incremental, { silent = true })
 vim.keymap.set("v", "<M-j>", ts_select.node_decremental, { silent = true })
-
+vim.keymap.set('x', 'p', '"_dP', { noremap = true })
 
 vim.keymap.set("v", "<leader>s", "<Esc>/\\%V", { silent = false })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "nix",
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.softtabstop = 2
+    vim.bo.tabstop = 2
+  end,
+})
