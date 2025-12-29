8f802f87b16a70d91b8cf822c795b9a42f7fed91
Dmitriy Sudakov <dsudakov@hyperad.tech>
2025-12-29 15:17:12 +0300

    fix

--- nixos/modules/zsh/zsh.nix
+++ nixos/modules/zsh/zsh.nix
@@ -10,25 +10,33 @@
 	    autosuggestion.enable = true;
 	    syntaxHighlighting.enable = true;
 
-        defaultKeymap = "emacs";
+      defaultKeymap = "emacs";
 
 	    initContent = builtins.readFile(pkgs.replaceVars ./zsh-extra.zsh {
-		  zshHistoryPlugin = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-		  p10kTheme = "${pkgs.zsh-fzf-history-search}/share/zsh-fzf-history-search/zsh-fzf-history-search.zsh";
-		  goto = "${pkgs.goto}/share/goto.sh";
+        p10kTheme= "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        zshHistoryPlugin  = "${pkgs.zsh-fzf-history-search}/share/zsh-fzf-history-search/zsh-fzf-history-search.zsh";
+        goto = "${pkgs.goto}/share/goto.sh";
 	    });
-	  };
-      home.packages = with pkgs; [
-        fzf
-        ripgrep
-        bat
-        fd
-        goto
 
-        zsh-fzf-history-search
-        zsh-autosuggestions
-        zsh-powerlevel10k
-      ];
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "history"
+        ];
+      };
+	  };
+
+    home.packages = with pkgs; [
+      fzf
+      ripgrep
+      bat
+      fd
+      goto
+
+      zsh-fzf-history-search
+      zsh-powerlevel10k
+    ];
 
 	  home.file.".p10k.zsh".source = ./p10k.zsh;
 	};
