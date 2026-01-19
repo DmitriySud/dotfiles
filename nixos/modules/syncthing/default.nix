d08d70888b3024abdc9a0a095b2972f40221f299
Dmitriy Sudakov <dmitriy.sudakov2001@yandex.ru>
2026-01-19 17:34:56 +0300

    add laptop-work device_id

--- nixos/modules/syncthing/default.nix
+++ nixos/modules/syncthing/default.nix
@@ -30,12 +30,17 @@ in
         devices = {
           desktop-personal.id = "YGZZGDV-LFSMJOD-OVR6FU6-J7BNAKD-ZGGMTOT-WDHMID6-HNO25CI-PDZGVAO";
           laptop-personal.id = "VRD6P5Y-WY4NNTO-DUEEKOG-TVMVD4R-HGOCC3S-SED45M6-QLTHDDP-2NYQ6QR";
+          laptop-work.id = "5BY6P5R-7CWSXMW-FXA6YNG-U7INCP7-H4QBIZL-T6Z3M2D-VOWRD55-7OAWTAR";
         };
 
         folders = {
           "goto-state" = {
             path = "${cfg.storage-dir}/goto";
-            devices = [ "desktop-personal" "laptop-personal" ];
+            devices = [ 
+              "desktop-personal" 
+              "laptop-personal" 
+              "laptop-work"
+            ];
             type = "sendreceive";
             versioning = {
               type = "simple";
